6d4233eb9fd7f94304952f060efd9aadf31fd4eb
Mishel Sandoval <analisk17@gmail.com>
2025-02-10 04:54:56 -0600

    Proyecto-final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { InfoComponent } from './info/info.component';
+import { OptionComponent } from './option/option.component';
+import { MoreComponent } from './more/more.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,MainComponent,InfoComponent,OptionComponent, MoreComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
